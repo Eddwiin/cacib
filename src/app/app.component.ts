16f5a678c2364ea7f8cd3d7094fcba6409d626ec
Maguran-Effy <maguran.mahendrarajan@effy.fr>
2023-08-17 23:27:56 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
+})
+export class AppComponent {
+  title = 'cacib';
+}
